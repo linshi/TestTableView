f21d7cf38ffa04a4542061decb992443a9cf196c
Lin <forrest.lin@sap.com>
2015-07-16 19:11:48 +0800

    set up for new test

--- TestTableView/ViewController.m
+++ TestTableView/ViewController.m
@@ -7,7 +7,6 @@
 //
 
 #import "ViewController.h"
-#import "HeaderView.h"
 #import "CellView.h"
 
 @interface ViewController (){
@@ -24,20 +23,7 @@
     
     items = @[@1,@7];
     
-    [[NSNotificationCenter defaultCenter] addObserverForName:@"UPDATE" object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *note) {
 
-        [self.tableView reloadData];
-        
-//        NSIndexPath *cellIndexPath =  [NSIndexPath indexPathForRow:0 inSection:0];
-//        [self.tableView beginUpdates];
-//        [self.tableView reloadRowsAtIndexPaths: [NSArray arrayWithObjects: cellIndexPath , nil] withRowAnimation: UITableViewRowAnimationNone];
-//        [self.tableView endUpdates];
-    }];
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
@@ -59,16 +45,6 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
     return 1;
-}
-
-- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
-    HeaderView *headerView = [[HeaderView alloc] initWithFrame:CGRectZero];
-    headerView.backgroundColor = [UIColor redColor];
-    return headerView;
-}
-
-- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
-    return 30;
 }
 
 @end
